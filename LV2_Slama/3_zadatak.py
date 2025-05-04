2ed2580b347ba8e6bf4e0dc4d9e6a3705e7305e9
SlaMax <157590758+SlaMax@users.noreply.github.com>
2025-05-04 19:13:10 +0200

    Update 3_zadatak.py

--- LV2_Slama/3_zadatak.py
+++ LV2_Slama/3_zadatak.py
@@ -1,8 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-img=plt.imread("road.jpg")
+image=plt.imread("road.jpg")
 
-posvjetljena=img+10
-plt.imshow(posvjetljena)
+image_float = image.astype(np.float32) / 255.0  
+brighter_image = image_float * 1.8
+brighter_image = np.clip(brighter_image, 0, 1)
+
+brighter_image_uint8 = (brighter_image * 255).astype(np.uint8)
+
+plt.imshow(brighter_image)
+plt.title("Posvijetljena slika")
+plt.show()
+
+
+width = len(image[0])  
+quarter_width = int(width/4)  
+
+plt.imshow(image[:, 1*quarter_width: 2*quarter_width , :])
+plt.show()
+
+
+rotated_image = np.rot90(image, 3)  
+plt.imshow(rotated_image)
+plt.show()
+
+
+flipped_image = np.flip(image, 0) 
+plt.imshow(flipped_image)
 plt.show()
